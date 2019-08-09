436b7d55b29ec0c25ec1e39c96583e2a11cf3b1c
mariamjaludi <mariam.jaludi@gmail.com>
2019-08-10 00:29:55 +0300

    delete button changed

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ class UI {
       <td>${book.title}</td>
       <td>${book.author}</td>
       <td>${book.isbn}</td>
-      <td><a href='#' class="btn btn-danger btn-sm delete">X</a></td>
+      <td><a href='#' class="delete"><i class="far fa-trash-alt"></i></a></td>
     `;
     list.appendChild(row);
   }
