e920bfeb17ab6e43fb23324913af30899d6e7ce5
mariamjaludi <mariam.jaludi@gmail.com>
2019-08-10 00:02:06 +0300

    books can be added and removed from local storage

--- app.js
+++ app.js
@@ -67,7 +67,34 @@ class UI {
 }
 
 //store class: handles storage
+class Store {
+  static getBooks(){
+    let books;
+     if(localStorage.getItem('books') === null){
+       books = [];
+     }
+     else {
+       books = JSON.parse(localStorage.getItem('books'));
+     }
+     return books;
+  }
 
+  static addBook(book){
+    const books = Store.getBooks();
+    books << book
+    localStorage.setItem(JSON.stringify(books))
+  }
+
+  static removeBook(isbn){
+    const books = Store.getBooks();
+    books.forEach((book, index)) => {
+      if(book.isbn === isbn){
+        books.splice(index, 1)
+      }
+    }
+    localStorage.setItem(JSON.stringify(books))
+  }
+}
 
 //event: display books
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
